fb8403d78db65149f2b6d2b8ce0f0fb2833bf53c
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-07-09 11:51:36 +0530

    Create 2) Creating new component.ts

---
+++ 2) The Basics/2) Creating new component.ts
@@ -0,0 +1,10 @@
+// Create Folder called 'server' inside 'app' folder and create file inside the server folder called 'server.component.ts' in that add this code.
+import { Component } from '@angular/core';  // Angular doesnt know about 'Component'. So, we should import it.
+
+@Component({    // To add some functionality
+  selector:'./server.component.html'  // we create one separate file inside 'server' called 'server.component.html' and write code inside it and link it to this file, using this code.
+})
+
+export class ServerComponent {    // To export the class to 'index.html' to display in web page
+
+}
