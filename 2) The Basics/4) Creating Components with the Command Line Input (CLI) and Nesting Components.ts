21d3cca2902feb9372aac92c0054fb36d90d63e3
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-07-12 13:04:53 +0530

    Create 4) Creating Components with the Command Line Input (CLI) and Nesting Components.ts

---
+++ 2) The Basics/4) Creating Components with the Command Line Input (CLI) and Nesting Components.ts
@@ -0,0 +1,7 @@
+// To create components using cli, select view on visual studio and click on terminal, then there type this command.
+ng generate component servers // or in simple way type "ng g c servers".
+
+
+// In server.component.html add
+<app-server></app-server>
+<app-server></app-server>
